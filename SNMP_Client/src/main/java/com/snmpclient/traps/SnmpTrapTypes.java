824816bc2c244637bd4c6c545df9fa5bda43b7e9
Michael Ramez <34134110+michaelramez@users.noreply.github.com>
2022-07-06 18:26:48 +0200

    client server upload

---
+++ SNMP_Client/src/main/java/com/snmpclient/traps/SnmpTrapTypes.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.snmpclient.traps;
+
+/**
+ *
+ * @author Michael Ramez
+ */
+public class SnmpTrapTypes {
+    public static final int NETWORK_CONNECTIVITY = 1;
+    public static final int HD_IS_FULL = 2;
+    public static final int MEMORY_IS_FULL = 3;
+}
---
+++ SNMP_Server/src/main/java/com/snmpserver/actions/ActionTypes.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.snmpserver.actions;
+
+/**
+ *
+ * @author Michael Ramez
+ */
+public class ActionTypes {
+    public static final int SMS = 1;
+    public static final int MAIL = 2;
+}
